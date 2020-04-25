cfc2adb41cd1502bf899d0b7d3b76e139258c94f
Danny <danny.yi.weng@gmail.com>
2020-04-25 17:16:17 -0400

    switch to GPRC

--- HeyNurse.py
+++ HeyNurse.py
@@ -18,6 +18,65 @@ from aiy.assistant.library import Assistant
 from aiy.board import Board, Led
 from aiy.voice import tts
 
+def power_off_pi():
+    tts.say('Good bye!')
+    subprocess.call('sudo shutdown now', shell=True)
+
+
+def reboot_pi():
+    tts.say('See you in a bit!')
+    subprocess.call('sudo reboot', shell=True)
+
+
+def say_ip():
+    ip_address = subprocess.check_output("hostname -I | cut -d' ' -f1", shell=True)
+    tts.say('My IP address is %s' % ip_address.decode('utf-8'))
+
+
+def hey_nurse():
+    tts.say('Hi, Danny')
+
+
+def process_event(assistant, led, event):
+    logging.info(event)
+    if event.type == EventType.ON_START_FINISHED:
+        led.state = Led.BEACON_DARK  # Ready.
+        print('Say "OK, Google" then speak, or press Ctrl+C to quit...')
+    elif event.type == EventType.ON_CONVERSATION_TURN_STARTED:
+        led.state = Led.ON  # Listening.
+    elif event.type == EventType.ON_RECOGNIZING_SPEECH_FINISHED and event.args:
+        print('You said:', event.args['text'])
+        text = event.args['text'].lower()
+        if text == 'power off':
+            assistant.stop_conversation()
+            power_off_pi()
+        elif text == 'reboot':
+            assistant.stop_conversation()
+            reboot_pi()
+        elif text == 'ip address':
+            assistant.stop_conversation()
+            say_ip()
+    elif event.type == EventType.ON_END_OF_UTTERANCE:
+        led.state = Led.PULSE_QUICK  # Thinking.
+    elif (event.type == EventType.ON_CONVERSATION_TURN_FINISHED
+          or event.type == EventType.ON_CONVERSATION_TURN_TIMEOUT
+          or event.type == EventType.ON_NO_RESPONSE):
+        led.state = Led.BEACON_DARK  # Ready.
+    elif event.type == EventType.ON_ASSISTANT_ERROR and event.args and event.args['is_fatal']:
+        sys.exit(1)
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    credentials = auth_helpers.get_assistant_credentials()
+    with Board() as board, Assistant(credentials) as assistant:
+        for event in assistant.start():
+            process_event(assistant, board.led, event)
+
+
+if __name__ == '__main__':
+    main()
 
 
 
@@ -70,9 +129,6 @@ if __name__ == '__main__':
     main()
 
 
-'''
-
-
 def get_hints(language_code):
     if language_code.startswith('en_'):
         return ('turn on the light',
@@ -86,20 +142,6 @@ def get_hints(language_code):
 def locale_language():
     language, _ = locale.getdefaultlocale()
     return language
-
-def power_off_pi():
-    tts.say('Good bye!')
-    subprocess.call('sudo shutdown now', shell=True)
-
-
-def reboot_pi():
-    tts.say('See you in a bit!')
-    subprocess.call('sudo reboot', shell=True)
-
-
-def say_ip():
-    ip_address = subprocess.check_output("hostname -I | cut -d' ' -f1", shell=True)
-    tts.say('My IP address is %s' % ip_address.decode('utf-8'))
 
 
 def process_event(assistant, led, event):
@@ -192,9 +234,8 @@ def main():
             print(event)
             process_event(assistant, board.led, event)
 
-    
-
 
 if __name__ == '__main__':
     main()
 
+'''
