5acc34f711bcbb6cb9cbd475b104569a5f3910fd
Danny <danny.yi.weng@gmail.com>
2020-04-25 17:37:13 -0400

    Update HeyNurse.py

--- HeyNurse.py
+++ HeyNurse.py
@@ -70,7 +70,7 @@ def process_event(assistant, led, event):
 
 
 def main():
-    logging.basicConfig(level=logging.INFO)
+    #logging.basicConfig(level=logging.INFO)
     logging.basicConfig(level=logging.DEBUG)
 
     credentials = auth_helpers.get_assistant_credentials()
