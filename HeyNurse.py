24336dd53be97089047b5359475a1dde8c6491b9
Danny <danny.yi.weng@gmail.com>
2020-05-05 23:32:31 -0400

    Added Parsing on Google Fit data

--- HeyNurse.py
+++ HeyNurse.py
@@ -5,83 +5,95 @@
 # Reference:
 # https://github.com/dannyweng
 
-import argparse
-import locale
-import logging
-import platform
-import subprocess
-import sys
-from aiy.board import Board, Led
-from aiy.assistant import auth_helpers
-from aiy.assistant.library import Assistant
-from aiy.voice import tts
-from google.assistant.library.event import EventType
-#from aiy.cloudspeech import CloudSpeechClient
+def runHeyNurse():
+
+    import argparse
+    import locale
+    import logging
+    import platform
+    import subprocess
+    import sys
+    from aiy.board import Board, Led
+    from aiy.assistant import auth_helpers
+    from aiy.assistant.library import Assistant
+    from aiy.voice import tts
+    from google.assistant.library.event import EventType
+    #from aiy.cloudspeech import CloudSpeechClient
+    import fitParse
 
 
-def power_off_pi():
-    tts.say('Good bye!')
-    subprocess.call('sudo shutdown now', shell=True)
+    def power_off_pi():
+        tts.say('Good bye!')
+        subprocess.call('sudo shutdown now', shell=True)
 
 
-def reboot_pi():
-    tts.say('See you in a bit!')
-    subprocess.call('sudo reboot', shell=True)
+    def reboot_pi():
+        tts.say('See you in a bit!')
+        subprocess.call('sudo reboot', shell=True)
 
 
-def say_ip():
-    ip_address = subprocess.check_output("hostname -I | cut -d' ' -f1", shell=True)
-    tts.say('My IP address is %s' % ip_address.decode('utf-8'))
+    def say_ip():
+        ip_address = subprocess.check_output("hostname -I | cut -d' ' -f1", shell=True)
+        tts.say('My IP address is %s' % ip_address.decode('utf-8'))
 
 
-def hey_nurse():
-    tts.say('Hi, Danny')
+    def hey_nurse():
 
 
-def process_event(assistant, led, event):
-    logging.info(event)
-    if event.type == EventType.ON_START_FINISHED:
-        led.state = Led.BEACON_DARK  # Ready.
-        print('Say "OK, Google" then speak, or press Ctrl+C to quit...')
-    elif event.type == EventType.ON_CONVERSATION_TURN_STARTED: #Hot Word activation 'OK Google'
-        led.state = Led.ON  # Listening.
-    elif event.type == EventType.ON_RECOGNIZING_SPEECH_FINISHED and event.args:
-        print('You said:', event.args['text'])
-        text = event.args['text'].lower()
-        if text == 'power off':
-            assistant.stop_conversation()
-            power_off_pi()
-        elif text == 'reboot':
-            assistant.stop_conversation()
-            reboot_pi()
-        elif text == 'ip address':
-            assistant.stop_conversation()
-            say_ip()
-        elif text == 'hey nurse':
-            assistant.stop_conversation()
-            hey_nurse()
-        else: #debug
-            #print('debug: ' + text)
-            print(EventType.ON_RENDER_RESPONSE)
-    elif event.type == EventType.ON_END_OF_UTTERANCE:
-        led.state = Led.PULSE_QUICK  # Thinking.
-    elif (event.type == EventType.ON_CONVERSATION_TURN_FINISHED
-        or event.type == EventType.ON_CONVERSATION_TURN_TIMEOUT
-        or event.type == EventType.ON_NO_RESPONSE):
-        led.state = Led.BEACON_DARK  # Ready.
-    elif event.type == EventType.ON_ASSISTANT_ERROR and event.args and event.args['is_fatal']:
-        sys.exit(1)
+    def googleFitHeart(date, dataType):
+        fitParse.googleFitAverage(date, dataType)
+        tts.say('Hi, Danny')
 
 
-def main():
-    logging.basicConfig(level=logging.INFO)
-    #logging.basicConfig(level=logging.DEBUG)
+    def process_event(assistant, led, event):
+        logging.info(event)
+        if event.type == EventType.ON_START_FINISHED:
+            led.state = Led.BEACON_DARK  # Ready.
+            print('Say "OK, Google" then speak, or press Ctrl+C to quit...')
+        elif event.type == EventType.ON_CONVERSATION_TURN_STARTED: #Hot Word activation 'OK Google'
+            led.state = Led.ON  # Listening.
+        elif event.type == EventType.ON_RECOGNIZING_SPEECH_FINISHED and event.args:
+            print('You said:', event.args['text'])
+            text = event.args['text'].lower()
+            if text == 'power off':
+                assistant.stop_conversation()
+                power_off_pi()
+            elif text == 'reboot':
+                assistant.stop_conversation()
+                reboot_pi()
+            elif text == 'ip address':
+                assistant.stop_conversation()
+                say_ip()
+            elif text == 'hey nurse':
+                assistant.stop_conversation()
+                hey_nurse()
+            elif text == 'date' & 'heart':
+                assistant.stop_conversation()
+                googleFitHeart()
+            else: #debug
+                #print('debug: ' + text)
+                print(EventType.ON_RENDER_RESPONSE)
+        elif event.type == EventType.ON_END_OF_UTTERANCE:
+            led.state = Led.PULSE_QUICK  # Thinking.
+        elif (event.type == EventType.ON_CONVERSATION_TURN_FINISHED
+            or event.type == EventType.ON_CONVERSATION_TURN_TIMEOUT
+            or event.type == EventType.ON_NO_RESPONSE):
+            led.state = Led.BEACON_DARK  # Ready.
+        elif event.type == EventType.ON_ASSISTANT_ERROR and event.args and event.args['is_fatal']:
+            sys.exit(1)
 
-    credentials = auth_helpers.get_assistant_credentials()
-    with Board() as board, Assistant(credentials) as assistant:
-        for event in assistant.start():
-            process_event(assistant, board.led, event)
+
+    def main():
+        logging.basicConfig(level=logging.INFO)
+        #logging.basicConfig(level=logging.DEBUG)
+
+        credentials = auth_helpers.get_assistant_credentials()
+        with Board() as board, Assistant(credentials) as assistant:
+            for event in assistant.start():
+                process_event(assistant, board.led, event)
 
 
-if __name__ == '__main__':
+    if __name__ == '__main__':
     main()
+
+runHeyNurse()
--- fitParse.py
+++ fitParse.py
@@ -7,10 +7,10 @@ import numpy as np
 
 FIT_DIRECTORY = "Fit/Daily Aggregations/"
 # Data Points that might be relevent
-# data_keys = 'Distance (m),Average heart rate (bpm),Max heart rate (bpm),Min heart rate (bpm),Average speed (m/s),Step count,Sleep duration (ms),Deep sleeping duration (ms),REM sleeping duration (ms)'.split(',')
+data_keys = 'Distance (m),Average heart rate (bpm),Max heart rate (bpm),Min heart rate (bpm),Average speed (m/s),Step count,Sleep duration (ms),Deep sleeping duration (ms),REM sleeping duration (ms)'.split(',')
 
 # Testing with critical data only
-data_keys = 'Distance (m),Average heart rate (bpm),Max heart rate (bpm),Min heart rate (bpm)'.split(',')
+# data_keys = 'Distance (m),Average heart rate (bpm),Max heart rate (bpm),Min heart rate (bpm)'.split(',')
 
 fit_data = []
 for file in os.listdir(FIT_DIRECTORY)[:-2]:
@@ -35,12 +35,52 @@ for file in os.listdir(FIT_DIRECTORY)[:-2]:
 
 df = pd.DataFrame(fit_data)
 # print(df)
-df.index = df['datetime']
+# df.index = df['datetime']
+# print(df)
 # print(df.index)
-df['time'] = pd.to_datetime(df['datetime'], format='%H:%M').dt.time
+
+# Create a time column
+# df['time'] = pd.to_datetime(df['datetime'], format='%H:%M').dt.time
 # print(df['time'])
 
+# Create a date column
+# df['date'] = pd.to_datetime(df['datetime'], format='%d').dt.date
+# print(df['date'])
 
 # Replace all NaN references with 0's
 df.fillna(0, inplace=True)
-print(df)
+
+# Set datetime as the INDEX otherwise INDEX will be 0,1,2,3,4...
+df = df.set_index(['datetime'])
+# print(df)
+
+# Use df.loc to query the rows you want
+# print(df.loc['2020-04-01'])
+# print(df.loc['2020-04-01':'2020-04-02'])
+# print(df.loc['2020-04-10 00:00:00': '2020-04-11 00:00:00'])
+# print(df.index)
+# print(df.columns)
+# print(sum(df.at['2020-04-01', 'Average heart rate (bpm)']))
+# print(df.at['2020-04-01', 'Average heart rate (bpm)'])
+# print(df.at['2020-04-01', 'Average heart rate (bpm)'])
+# print(df.count(axis='columns'))
+
+
+
+# Two methods to get the MEAN
+# countData = df.at['2020-04-01', 'Average heart rate (bpm)']
+
+# num_zeros = (countData == 0).sum()
+# num_ones = (countData != 1).sum()
+# print(sum(df.at['2020-04-01', 'Average heart rate (bpm)']) / (num_ones+num_zeros))
+
+# print(np.mean(countData))
+
+def googleFitAverage(date, dataType):
+    countData = df.at[f'{date}', f'{dataType}']
+    # print(np.mean(countData))
+    return np.mean(countData)
+
+
+# print(googleFitAverage('2020-04-01', 'Average heart rate (bpm)'))
+# print(googleFitAverage('2020-04-01', 'Distance (m)'))
